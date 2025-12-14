b479febe5177146e5013d196d2736bdb9957f78b
Juan Cruz <juancasserab@gmail.com>
2025-12-14 15:28:16 -0300

    Resolución de la práctica 9.

---
+++ Práctica/Resolución práctica 9/engine.py
@@ -0,0 +1,243 @@
+# Calcado de Micrograd (https://github.com/karpathy/micrograd/blob/master/micrograd/engine.py)
+import math
+
+
+class Value:
+    """
+    Representa a un valor escalar y su gradiente.
+
+    Este valor es el elemento básico de cada neurona
+    de la red neuronal y soporta operaciones aritméticas
+    y funciones matemáticas necesarias para el
+    cálculo del forward y backward pass.
+    """
+
+    def __init__(
+        self,
+        data: float,
+        _children: tuple = (),
+        _op: str = "",
+        name: str = "",
+    ) -> None:
+        """
+        - El argumento 'data' es el valor escalar que representa este objeto Value.
+            - Puede ser un número entero o un número de punto flotante.
+        - El argumento '_children' es una tupla de objetos Value. Representan
+        a los nodos padres en el grafo.
+        - El argumento '_op' es una cadena que representa la operación que
+        creó a este nodo (+, *, etc). Útil para graphviz.
+        - El argumento 'name' es una cadena que representa el nombre
+        de este nodo. Útil para graphviz.
+        """
+        self.data = data
+        self.grad = 0
+        self.name = name
+
+        # Función que calcula el gradiente durante el backward pass.
+        self._backward = lambda: None
+
+        # Conjunto de nodos padres en el grafo.
+        self._prev = set(_children)
+
+        # La operación que creó a este nodo. Útil para graphviz.
+        self._op = _op
+
+    def __add__(self, other: "Value") -> "Value":
+        """
+        - El argumento 'other' es otro objeto Value.
+        - Define cómo se suman dos objetos Value:
+            - Se crea un nuevo objeto Value cuyo 'data' es la suma
+            de los valores de los dos objetos, y cuyos padres son
+            los dos objetos que se están sumando. La operación es "+".
+            - Se define la función _backward para calcular los gradientes
+            durante el backward pass.
+        """
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(data=self.data + other.data, _children=(self, other), _op="+")
+
+        def _backward() -> None:
+            """
+            Define el backward pass para la suma.
+
+            El gradiente se propaga igual a ambos padres.
+            """
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other: "Value | float") -> "Value":
+        """
+        - El argumento 'other' es otro objeto Value.
+        - Define cómo se multiplican dos objetos Value:
+            - Se crea un nuevo objeto Value cuyo 'data' es el producto
+            de los valores de los dos objetos, y cuyos padres son
+            los dos objetos que se están multiplicando. La operación es "*".
+            - Se define la función _backward para calcular los gradientes
+            durante el backward pass.
+        """
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(data=self.data * other.data, _children=(self, other), _op="*")
+
+        def _backward() -> None:
+            """
+            Define el backward pass para la multiplicación.
+
+            Ejemplo: En 6 * 5, la derivada de 6 con respecto a 5 es 6,
+            y la derivada de 5 con respecto a 6 es 5.
+            """
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other: float) -> "Value":
+        """
+        - El argumento 'other' es un número entero o float.
+        - Define cómo se eleva un objeto Value a un entero o float:
+            - Se crea un nuevo objeto Value cuyo 'data' es self.data ** other.
+            La operación es "**".
+            - Se define la función _backward para calcular los gradientes
+            durante el backward pass.
+        """
+        assert isinstance(other, (int, float)), (
+            "only supporting int/float powers for now"
+        )
+        out = Value(data=self.data**other, _children=(self,), _op=f"**{other}")
+
+        def _backward() -> None:
+            """
+            Define el backward pass para la potenciación.
+            La derivada de x^n es n * x^(n - 1).
+            """
+            self.grad += (other * (self.data ** (other - 1))) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def relu(self) -> "Value":
+        """
+        - Define la función ReLU (Rectified Linear Unit):
+            - Crea un nuevo objeto Value cuyo 'data' es 0 si self.data es
+            menor a cero, o self.data en caso contrario.
+        """
+        out = Value(
+            data=0.0 if self.data < 0 else self.data,
+            _children=(self,),
+            _op="ReLU",
+        )
+
+        def _backward() -> None:
+            """
+            Define el backward pass para la función ReLU.
+            La derivada de ReLU es 1 si x > 0, y 0 en caso contrario.
+            """
+            self.grad += (out.data > 0) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def exp(self) -> "Value":
+        """
+        - Define la función exponencial:
+            - Crea un nuevo objeto Value cuyo 'data' es e elevado a
+            self.data.
+        """
+        x = self.data
+        out = Value(data=math.exp(x), _children=(self,), _op=f"e^{self.data}")
+
+        def _backward() -> None:
+            """
+            Define el backward pass para la función exponencial.
+            La derivada de e^x es e^x.
+            """
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def backward(self) -> None:
+        """
+        Realiza el backward pass, empezando desde el nodo actual
+        pasando por todos los nodos padres de él en el grafo.
+        """
+        topo = []
+        visited = set()
+
+        def build_topo(v: Value) -> None:
+            """
+            Construye el orden topológico de todos los nodos en el grafo
+            de forma recursiva.
+            """
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        # Recorremos los nodos en orden topológico inverso, es decir, primero
+        # el nodo final, que en una red neuronal es la función de pérdida,
+        # y luego todos los nodos anteriores hasta llegar finalmente a las
+        # entradas de la red.
+        # Cada Value tiene su propia función _backward que varía según
+        # qué operación fue la que lo creó.
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
+
+    def __neg__(self) -> "Value":  # -self
+        """
+        Define la negación de un objeto Value.
+        """
+        return self * -1
+
+    def __radd__(self, other: "Value") -> "Value":  # other + self
+        """
+        Define la suma entre un objeto Value y otro objeto.
+        """
+        return self + other
+
+    def __sub__(self, other: "Value") -> "Value":  # self - other
+        """
+        Define la resta entre un objeto Value y otro objeto.
+        """
+        return self + (-other)
+
+    def __rsub__(self, other: "Value") -> "Value":  # other - self
+        """
+        Define la resta invertida entre un objeto Value y otro objeto.
+        """
+        return other + (-self)
+
+    def __rmul__(self, other: "Value") -> "Value":  # other * self
+        """
+        Define la multiplicación entre un objeto Value y otro.
+        """
+        return self * other
+
+    def __truediv__(self, other: "Value") -> "Value":  # self / other
+        """
+        Define la división entre un objeto Value y otro.
+        """
+        return self * other**-1
+
+    def __rtruediv__(self, other: "Value") -> "Value":  # other / self
+        """
+        Define la división entre un objeto Value y otro.
+        """
+        return other * self**-1
+
+    def __repr__(self) -> str:
+        """
+        Representación en string del objeto Value.
+        """
+        return f"Value(data={self.data}, grad={self.grad}, name={self.name})"
---
+++ Práctica/Resolución práctica 9/nn_refactored.py
@@ -0,0 +1,230 @@
+import random
+
+from michigrad.engine_extended import Value
+
+
+class Module:
+    """
+    Contiene dos métodos que todos los componentes principales
+    de la red neuronal (la red misma, las capas, las neuronas) implementan.
+    """
+
+    def zero_grad(self) -> None:
+        """
+        Reiniciar los gradientes de todos los parámetros a cero.
+        Se hace antes de hacer backpropagation en cada iteración de entrenamiento.
+        """
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self) -> list[Value]:
+        """
+        Devuelve todos los parámetros del módulo, que
+        están representados como objetos Value.
+        """
+        return []
+
+
+class Neuron(Module):
+    """
+    Representa a una neurona de una capa de la red neuronal.
+    """
+
+    def __init__(self, nin: int) -> None:
+        """
+        - El argumento 'nin' indica la cantidad de pesos de la neurona.
+        - A su vez, la neurona tiene un único sesgo (bias) que se
+        suma a la salida lineal.
+
+        Al crearse, la neurona crea 'nin' pesos, inicializados con
+        valores aleatorios entre -1 y 1. Además, inicializa su sesgo en 0.
+        """
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(0)
+
+    def __call__(self, x: list[Value]) -> Value:
+        """
+        - Representa el forward pass de la neurona, es decir, cómo
+        calcula su salida a partir de las entradas x.
+        - El argumento 'x' es una lista de objetos Value que representan
+        las entradas a la neurona.
+        - La salida de la neurona es un objeto Value que se calcula como
+        la suma ponderada de las entradas más el sesgo. Es decir,
+        una sumatoria de cada entrada multiplicada por su peso
+        correspondiente, y al obtener la suma total, se le suma el sesgo.
+        """
+        return sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+
+    def parameters(self) -> list[Value]:
+        """
+        Devuelve una lista de objetos Value que representan a
+        todos los parámetros de la neurona: todos sus pesos, y
+        además el sesgo.
+        """
+        return self.w + [self.b]
+
+    def __repr__(self) -> str:
+        """
+        Representación en string de la neurona.
+        """
+        return f"{'Linear'}Neuron({len(self.w)})"
+
+
+class ReLU(Module):
+    """
+    Representa a una capa de la red neuronal que aplica la función de activación
+    ReLU a cada una de sus entradas.
+    """
+
+    def __call__(self, x: list[Value]) -> Value | list[Value]:
+        """
+        - Representa el forward pass de la capa, es decir, cómo
+        calcula su salida a partir de las entradas x.
+        - El argumento 'x' es una lista de objetos Value que representan
+        las entradas a la capa.
+        - La salida de la capa es una lista de objetos Value, donde
+        a cada entrada se le ha aplicado la función ReLU.
+        """
+        out = [xi.relu() for xi in x]
+        return out[0] if len(out) == 1 else out
+
+
+class Tanh(Module):
+    """
+    Representa a una capa de la red neuronal que aplica la función de activación
+    tangente hiperbólica (tanh) a cada una de sus entradas.
+    """
+
+    def __call__(self, x: list[Value]) -> Value | list[Value]:
+        """
+        - Representa el forward pass de la capa, es decir, cómo
+        calcula su salida a partir de las entradas x.
+        - El argumento 'x' es una lista de objetos Value que representan
+        las entradas a la capa.
+        - La salida de la capa es una lista de objetos Value, donde
+        a cada entrada se le ha aplicado la función tanh.
+        """
+        out = [xi.tanh() for xi in x]
+        return out[0] if len(out) == 1 else out
+
+
+class Sigmoid(Module):
+    """
+    Representa a una capa de la red neuronal que aplica la función de activación
+    sigmoide a cada una de sus entradas.
+    """
+
+    def __call__(self, x: list[Value]) -> Value | list[Value]:
+        """
+        - Representa el forward pass de la capa, es decir, cómo
+        calcula su salida a partir de las entradas x.
+        - El argumento 'x' es una lista de objetos Value que representan
+        las entradas a la capa.
+        - La salida de la capa es una lista de objetos Value, donde
+        a cada entrada se le ha aplicado la función sigmoide.
+        """
+        out = [xi.sigmoid() for xi in x]
+        return out[0] if len(out) == 1 else out
+
+
+class Layer(Module):
+    """
+    Representa a una capa de la red neuronal. Puede tener
+    una o más neuronas.
+    """
+
+    def __init__(self, nin: int, nout: int) -> None:
+        """
+        - El argumento 'nin' indica la cantidad de pesos que cada
+        neurona de la capa debe tener.
+        - El argumento 'nout' indica la cantidad de neuronas que
+        debe tener la capa.
+        - El argumento '**kwargs' permite pasar argumentos a cada
+        neurona al momento de crearlas. En este caso, el único argumento
+        adicional posible es 'nonlin', que indica si la neurona debe aplicar
+        o no una función de activación no lineal (ReLU) a su salida.
+
+        Al crearse, la capa crea 'nout' neuronas, cada una con 'nin' pesos.
+        """
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x: list[Value]) -> Value | list[Value]:
+        """
+        - Representa el forward pass de la capa, es decir, cómo
+        calcula su salida a partir de las entradas x.
+        - El argumento 'x' es una lista de objetos Value que representan
+        las entradas a la capa.
+        - La salida de la capa depende de la cantidad de neuronas que tenga:
+          - Si tiene una única neurona, la salida es el resultado de
+            pasarle la lista de entradas 'x' a esa neurona, lo cual es
+            un objeto Value.
+          - Si tiene más de una neurona, la salida es el resultado de
+            pasarle la lista de entradas 'x' a cada una de las
+            neuronas una por una, lo cual es una lista de objetos Value.
+        """
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self) -> list[Value]:
+        """
+        Devuelve una lista de objetos Value que representan a
+        todos los parámetros de la capa. Para esto, le pide a cada
+        una de sus neuronas que devuelva SUS parámetros y junta a todos
+        esos parámetros en una única lista.
+        """
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self) -> str:
+        """
+        Representación en string de la capa.
+        """
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+
+class MLP(Module):
+    """
+    Representa a una red neuronal que puede tener una o más capas.
+    """
+
+    def __init__(self, nin: int, nouts: list[int]) -> None:
+        """
+        - El argumento 'nin' indica la cantidad de pesos que debe tener
+        cada neurona de la PRIMERA capa de la red.
+        - El argumento 'nouts' es una lista de enteros que indica
+        la cantidad de neuronas que debe tener cada capa de la red.
+        - La red neuronal tiene una lista de capas.
+        """
+        sz = [nin] + nouts
+        self.layers = [Layer(nin=sz[i], nout=sz[i + 1]) for i in range(len(nouts))]
+
+    def __call__(self, x: list[Value]) -> Value | list[Value]:
+        """
+        - Representa el forward pass de la red neuronal, es decir, cómo
+        calcula su salida a partir de las entradas x.
+        - El argumento 'x' es una lista de objetos Value que representan
+        las entradas a la red neuronal.
+        - La salida de la red neuronal depende de la cantidad de capas que tenga:
+          - Si tiene una única capa, la salida es el resultado de pasarle la lista de
+            entradas 'x' a esa capa, lo cual es un objeto Value.
+          - Si tiene más de una capa, la salida es el resultado de pasarle la lista de
+            entradas 'x' a cada una de las capas una por una, lo cual es una lista de
+            objetos Value.
+        """
+        for layer in self.layers:
+            z = layer(x)
+        return z
+
+    def parameters(self) -> list[Value]:
+        """
+        Devuelve una lista de objetos Value que representan a
+        todos los parámetros de la red neuronal. Para esto, le pide a cada
+        una de sus capas que devuelva SUS parámetros, y junta a todos
+        esos parámetros en una única lista.
+        """
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self) -> str:
+        """
+        Representación en string de la red neuronal.
+        """
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
